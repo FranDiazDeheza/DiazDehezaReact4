a807f12a0efe3f97922892adfebe88ff2fc9bace
Fran Diaz Deheza <84636613+FranDiazDeheza@users.noreply.github.com>
2022-03-05 07:30:39 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import './App.css';
+import Navbar from './components/navBar';
+import ItemListConteiner from './components/ItemListConteiner';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import ItemDetailConteiner from './components/ItemDetail/ItemDetailConteiner';
+import SinCarrito from './components/sincarrito';
+
+
+function App() {
+  
+  
+  return (
+    <>
+  <Router>
+ 
+  <Navbar />
+  <Routes>
+    <Route path="/" element={<ItemListConteiner greeting="Libros"/>} />
+    <Route path="/category/:categoryid" element={<ItemListConteiner greeting="Categoria de Libros"/>} />
+    <Route path="/item/:id" element={<ItemDetailConteiner greeting="Detalles"/>} />
+    <Route path="/carrito" element={<SinCarrito></SinCarrito>} />
+
+  </Routes>
+ 
+ </Router>  
+
+
+ </>
+  );
+}
+
+
+export default App;
---
+++ src/components/sincarrito.jsx
@@ -0,0 +1,18 @@
+
+import react from "react"
+
+const SinCarrito = () => {
+    return (
+        <div>
+
+        <h1>Todavia en Produccion</h1>
+
+        <img src="https://media3.giphy.com/avatars/moodman/ulSuW7fRIM5X.gif" alt=""></img>
+    
+        </div>
+    )
+}
+
+
+
+export default SinCarrito
